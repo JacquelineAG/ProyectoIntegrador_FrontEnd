88b55105252fb5c5df7a98a4050e44ad8c1081cc
Jacqueline <107328708+JacquelineAG@users.noreply.github.com>
2022-06-12 19:54:15 -0300

    Agregado de BootStrap a Header. Eliminación de RS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-AP/logo-ap.component';
-import { RSComponent } from './components/rs/rs.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { InfoComponent } from './components/info/info.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RSComponent,
     BannerComponent,
     InfoComponent,
     ExperienciaComponent,
