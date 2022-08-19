14faa273cbf05f2864764959cbd0fbbefa9ad5ee
Jacqueline <107328708+JacquelineAG@users.noreply.github.com>
2022-08-19 00:23:31 -0300

    FrontEnd tablas

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404Component } from './components/error404/error404.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'login', component: LoginComponent},
-  {path:'error404', component: Error404Component}
+  {path:'error404', component: Error404Component},
+  {path: 'nuevaexp', component: NewExperienciaComponent},
+  {path:'editexp/:id', component:EditExperienciaComponent}
 ];
 
 @NgModule({
