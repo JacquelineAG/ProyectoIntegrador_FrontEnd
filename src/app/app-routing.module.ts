32958ae17f33f0da32b66f96715599c8d82ea19f
Jacqueline <107328708+JacquelineAG@users.noreply.github.com>
2022-08-24 00:27:33 -0300

    FrontEnd fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditcursosComponent } from './components/cursos/editcursos.component';
 import { NewcursosComponent } from './components/cursos/newcursos.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { Error404Component } from './components/error404/error404.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
+import { EditidiomaComponent } from './components/idiomas/editidioma.component';
 import { NewidiomaComponent } from './components/idiomas/newidioma.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -17,7 +20,10 @@ const routes: Routes = [
   {path:'editexp/:id', component:EditExperienciaComponent},
   {path:'nuevaedu', component: NeweducacionComponent},
   {path:'nuevoidi', component: NewidiomaComponent},
-  {path:'nuevocur', component: NewcursosComponent}
+  {path:'nuevocur', component: NewcursosComponent},
+  {path:'editedu/:id', component:EditeducacionComponent},
+  {path:'editcur/:id', component:EditcursosComponent},
+  {path:'editidi/:id', component:EditidiomaComponent}
 ];
 
 @NgModule({
